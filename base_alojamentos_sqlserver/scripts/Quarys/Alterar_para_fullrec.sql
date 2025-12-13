ad3fc6e87330bc2c78b42518f727d0f3d2205083
Miguel <142721775+miguele618@users.noreply.github.com>
2025-12-13 17:26:55 +0000

    Delete base_alojamentos_sqlserver directory

--- base_alojamentos_sqlserver/scripts/Quarys/Alterar_para_fullrec.sql
+++
@@ -1,8 +0,0 @@
-USE master;
-GO
-
--- Muda o modelo de recuperação para COMPLETO (Permite backups de Log)
-ALTER DATABASE AluguerHab SET RECOVERY FULL;
-GO
-
-PRINT 'Base de dados alterada para FULL RECOVERY MODEL.';
--- base_alojamentos_sqlserver/scripts/Quarys/Teste.sql
+++
@@ -1,6 +0,0 @@
-DECLARE @PropId INT = (SELECT TOP 1 PropriedadeId FROM core.Propriedade);
--- Tentar reservar em Fevereiro (Livre)
-EXEC core.SP_CheckDisponibilidade 
-    @PropriedadeId = @PropId, 
-    @CheckIn = '2025-02-01', 
-    @CheckOut = '2025-02-05';
